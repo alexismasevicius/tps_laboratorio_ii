a2958d2b538591bf93b6a6ee158425228261d11f
alexis m <alexismasevicius@gmail.com>
2021-09-18 14:36:39 -0300

    TP 1 de labo II

---
+++ TP1/Entidades/Operando.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+
+        //CONSTRUCTORES
+
+        public Operando()
+        {
+            this.numero = 0;
+        }
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            
+            this.Numero = strNumero;
+            
+        }
+
+        ///PROPIEDAD
+        public string Numero 
+        { 
+            set 
+            { 
+                this.numero = ValidarOperando(value);
+            } 
+        }
+
+        //VALIDACIONES
+        
+        /// <summary>
+        /// Valida si un string se encuentra en formato binario
+        /// </summary>
+        /// <param name="binario">numero a validar</param>
+        /// <returns>True si es binario, False si no lo es</returns>
+        private static bool EsBinario(string binario)
+        {
+            foreach (char item in binario)
+            {
+                if (item !='0' && item!='1')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        /// <summary>
+        /// Valida si es numerico, en caso contrario devuelve 0
+        /// </summary>
+        /// <param name="strNumero">string a validar</param>
+        /// <returns>El numero en formato Double. Si no es numerico devuelve 0</returns>
+        private static double ValidarOperando(string strNumero)
+        {
+            double aux;
+
+            if(!Double.TryParse(strNumero, out aux))
+            {
+                return 0;
+            }
+            
+
+            return aux;
+        }
+
+
+        //OPERACIONES DE CONVERSION
+
+        /// <summary>
+        /// Convierte un numero binario a Decimal
+        /// </summary>
+        /// <param name="binario">string del numero</param>
+        /// <returns>El numero convertido a decimal. En caso de no recibir un numero binario, devuelve "Valor invalido"</returns>
+        public static string BinarioDecimal(string binario)
+        {
+            if (EsBinario(binario)==true)
+            {
+                int auxInt = Convert.ToInt32(binario,2);
+
+                return auxInt.ToString(); 
+            }
+            else
+            {
+                return "Valor Invalido";
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Convierte un numero decimal positivo en binario
+        /// </summary>
+        /// <param name="numero">numero a convertir</param>
+        /// <returns>El numero convertido en string. En caso de ser negativo devuelve "Error"</returns>
+        public static string DecimalBinario(double numero)
+        {
+                string auxStr;
+                int auxInt;
+            if(numero>0)
+            {
+                auxInt = Convert.ToInt32(numero); 
+                auxStr=Convert.ToString(auxInt, 2);
+            }
+            else
+            {
+                auxStr = "Error";
+            }
+
+
+            return auxStr;
+        }
+        
+
+        /// <summary>
+        /// Convierte un string numerico decimal en un string en binario
+        /// </summary>
+        /// <param name="numero">string numerico a convertir</param>
+        /// <returns>El numero convertido a binario. En caso de no ser numerico devuelve "Error"</returns>
+        public static string DecimalBinario(string numero)
+        {
+            double auxDouble;
+            string resultado = "Error";
+
+            if(Double.TryParse(numero, out auxDouble))
+            {
+                resultado=DecimalBinario(auxDouble);
+            }
+
+
+            return resultado;
+        }
+        
+
+        //SOBRECARGA DE OPERADORES
+
+        /// <summary>
+        /// Suma dos objetos de tipo operando
+        /// </summary>
+        /// <param name="n1">primer Operando</param>
+        /// <param name="n2">segundo Operando</param>
+        /// <returns>El resultado de suma de los dos objetos tipo operando</returns>
+        public static double operator +(Operando n1, Operando n2)
+        {
+            double retorno = n1.numero + n2.numero;
+            return retorno;
+        }
+        /// <summary>
+        /// Resta dos objetos de tipo operando
+
+        /// </summary>
+        /// <param name="n1">primer Operando</param>
+        /// <param name="n2">segundo Operando</param>
+        /// <returns>El resultado de la resta de los dos objetos tipo operando</returns>
+        public static double operator -(Operando n1, Operando n2)
+        {
+            double retorno = n1.numero - n2.numero;
+            return retorno;
+        }
+        /// <summary>
+        /// Multiplica dos objetos de tipo operando
+        /// </summary>
+        /// <param name="n1">primer Operando</param>
+        /// <param name="n2">segundo Operando</param>
+        /// <returns>Resultado de la multiplicación de los dos objetos tipo operando</returns>
+        public static double operator *(Operando n1, Operando n2)
+        {
+            double retorno = n1.numero * n2.numero;
+            return retorno;
+        }
+        /// <summary>
+        /// Divide dos objetos de tipo operando
+        /// </summary>
+        /// <param name="n1">primer Operando</param>
+        /// <param name="n2">segundo Operando</param>
+        /// <returns>El resultado de la división de los dos objetos tipo operando. En caso de dividir por 0 devuelve el minimo valor</returns>
+        public static double operator /(Operando n1, Operando n2)
+        {
+            double retorno;
+            if (n2.numero!=0)
+            {
+                retorno = n1.numero / n2.numero;
+            }
+            else
+            {
+                retorno = double.MinValue;
+            }
+            
+            return retorno;
+        }
+
+
+    }
+}
---
+++ TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        //INICIALIZACION
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        //METODOS 
+        private void Limpiar()
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lstOperaciones.Text = "";           ///esto se hace asi?
+            cmbOperador.Text = " ";
+            label1.Text = "Resultado";
+        }
+
+        private static double Operar (string numero1, string numero2, string operador)
+        {
+            char operadorAux;
+            double ret;
+
+            Operando op1 = new Operando(numero1);
+            Operando op2 = new Operando(numero2);
+
+            operadorAux = Convert.ToChar(operador);
+            ret = Calculadora.Operar(op1, op2, operadorAux);
+
+            return ret;
+        }
+
+
+        //BOTONES
+
+        //boton limpiar
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        //boton operar
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado;
+            string resultadoStr;
+
+
+            resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            resultadoStr= resultado.ToString();
+
+            //Agrega un item a la lista
+            if(cmbOperador.Text==" ")
+            {
+                lstOperaciones.Items.Add($"{txtNumero1.Text} + {txtNumero2.Text} = {resultadoStr}");
+
+            }
+            else
+            {
+                lstOperaciones.Items.Add($"{txtNumero1.Text} {cmbOperador.Text} {txtNumero2.Text} = {resultadoStr}");
+            }
+            
+            //muestra el resultado en label
+            if(resultadoStr.Length<10)
+            {
+                label1.Text = resultadoStr;
+            }
+            else
+            {
+                label1.Text = "Error";
+            }
+        }
+
+        //boton convertir a binario
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != "Resultado" && 
+                label1.Text.Length < 10
+                )
+            {
+                string binarioStr = Operando.DecimalBinario(label1.Text);
+                label1.Text = binarioStr;
+            }
+            else
+            {
+                label1.Text = "Error.";
+            }
+            
+        }
+
+        //boton convertir a decimal
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            string binarioStr=Operando.BinarioDecimal(label1.Text);
+            label1.Text=binarioStr;
+        }
+
+
+        //CIERRE DEL FORMULARIO
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Esta seguro de salir?", "Salida", MessageBoxButtons.YesNo)==DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        
+    }
+}
